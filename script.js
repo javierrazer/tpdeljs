6c36fae7a69d85888fc5a860449e1a0adc8db386
javierrazer <javierzetas422@gmail.com>
2022-10-06 05:20:14 -0300

    segundo commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var n1, n2, vsuma;
+
+n1 = parseInt(prompt('Ingresar numero 1'));
+n2 = parseInt(prompt('Ingresar numero 2'));
+
+vsuma = n1 + n2;
+document.write('La suma es ' + vsuma);
+
+let x = 10;
+let y = 20;
+
+let z = y - x;
+
+console.log(z);
